78b6f31fb653a188ea49222452bfd299ee41533d
Seth Adamson <seth.c.adamson@gmail.com>
2018-10-02 14:53:32 -0600

    eslint/ routes setup

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+
+export default (
+        <Switch>    
+            <Route component={Main} path='/' exact/>
+            <Route component={} path='' />
+        </Switch>
+)
