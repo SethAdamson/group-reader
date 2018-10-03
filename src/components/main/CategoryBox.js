65cb210f275d317bacbc4fdf97200eafed540938
Michael Thurman <michaelrt17@icloud.com>
2018-10-03 14:39:51 -0600

    built toggle wrapper

--- src/components/main/CategoryBox.js
+++ src/components/main/CategoryBox.js
@@ -2,14 +2,12 @@ import React, {Component} from 'react';
 
 export default class CategoryBox extends Component {
     render() {
-        // let mappedGroups = this.props.groups.map(group => {
-
-        // })
+        let {categoryTitle, categoryDesc, toggle, on} = this.props;
         if(this.props.boxType === 'category') {
             return (
-                <div className='basic-category-box'>
-                    <h3>Category Title</h3>
-                    <p>This is the category description. This is the category description.</p>
+                <div className='basic-category-box' onClick={toggle}>
+                    <h3>{categoryTitle}</h3>
+                    <p className='category-desc' style={on ? {display: 'inherit'} : {display: 'none'}}>{categoryDesc}</p>
                 </div> 
             )
         }
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 import CategoryBox from './CategoryBox';
+import Toggle from '../ToggleRPC';
+import _ from 'lodash';
 
 export default class Main extends Component {
   render() {
+    let x = _.uniqBy(this.props.allByCategory, (e) => e.category_id).map(category => {
+      return (
+        <Toggle>
+          {({on, toggle}) => (
+          <div>
+            <CategoryBox on={on} toggle={toggle} boxType='category' categoryTitle={category.category_name} categoryDesc={category.category_desc} key={category.category_id}/>
+            {on && <CategoryBox boxType='groupsByCategory' />}
+          </div> 
+        )}
+        </Toggle> 
+      )
+    })
+    console.log(this.props.allByCategory, x)
     return (
       <div>
-        <CategoryBox boxType='category'/>
-        <CategoryBox boxType='category'/>
-        <CategoryBox boxType='category'/>
-        <CategoryBox boxType='category'/>
-        <CategoryBox boxType='category'/>
-        <CategoryBox boxType='category'/>
+        {x}
       </div> 
     )
   }
