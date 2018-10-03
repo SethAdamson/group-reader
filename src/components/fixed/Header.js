95028c4cd5b58366e9dcaa66b3db045bae3d2f3b
Seth Adamson <seth.c.adamson@gmail.com>
2018-10-03 13:55:39 -0600

    header setup

--- src/components/fixed/Header.js
+++ src/components/fixed/Header.js
@@ -1,13 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import defaultProfile from '../../media/defaultProfile.png';
 
-export default class Header extends Component {
-  constructor() {
-    super();
-  }
-
-  componentDidMount() {}
-
-  render() {
-    return <div>Header</div>;
-  }
+export default function Header() {
+  return (
+    <div className="header-outer">
+      <span className="header-menu">
+        <h3>
+          Menu
+          <FontAwesomeIcon icon="bars" style={{ position: 'relative', top: '2px' }} />
+        </h3>
+      </span>
+      <span className="header-title">
+        <h1>Same Page</h1>
+      </span>
+      <span className="header-profile">
+        <img className="header-img" src={defaultProfile} alt="profile" />
+      </span>
+    </div>
+  );
 }
