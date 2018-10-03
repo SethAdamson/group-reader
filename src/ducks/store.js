eb3be05fc4fb6b2d5a8a681308d29e7c4530dc08
Seth Adamson <seth.c.adamson@gmail.com>
2018-10-03 12:09:02 -0600

    main container setup

--- src/ducks/store.js
+++ src/ducks/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
-import reducer from './reducer';
 import promiseMiddleware from 'redux-promise-middleware';
+import reducer from './reducer';
 
 export default createStore(reducer, applyMiddleware(promiseMiddleware()));
